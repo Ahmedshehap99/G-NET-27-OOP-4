e6ce975a79bc4c74210370c43ffed3c6bb4ab1d0
AhmedShehap99 <55rrtyy7@gmail.com>
2026-04-03 06:29:14 +0100

    PART 01

--- OOP Assignment 4/Program.cs
+++ OOP Assignment 4/Program.cs
@@ -4,7 +4,55 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region PART 01
+            //// Part 01
+
+
+            //  Question 01 :
+
+            //          Static Binding(Early Binding) 
+            //                 =>  compile time
+
+
+
+
+            //          Dynamic Binding(Late Binding)
+            //                    => run time
+
+
+
+
+
+            //  Question 02 :
+
+            //          Method Overloading : Same method name, different parameter lists — within the same class. Resolved at compile time.
+
+
+
+            //          Method Overriding : Derived class overrides a virtual method from the base class using the override keyword.
+
+
+
+
+            // Question 03 :
+
+
+
+            //            virtual → Allows a method to be override in a child class.
+
+            //            override → Used in a child class to redefine a method.
+
+            //            sealed → Prevents any other class from overriding it.
+
+            #endregion
+
+
+
+
+
+
+
         }
     }
 }
