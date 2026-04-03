8f29f49d4bbd044810eff755a7957338bc8dfa95
AhmedShehap99 <55rrtyy7@gmail.com>
2026-04-03 06:35:19 +0100

    PART 02

--- OOP Assignment 4/Program.cs
+++ OOP Assignment 4/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace OOP_Assignment_4
+﻿using System.Net.Sockets;
+
+namespace OOP_Assignment_4
 {
     internal class Program
     {
@@ -45,6 +47,51 @@
 
             //            sealed → Prevents any other class from overriding it.
 
+            #endregion
+
+            #region PART 02
+            namespace Assignment_04_OOP.BookingTicket.Inheritance
+    {
+        internal class Cinema
+        {
+
+            private Ticket[] tickets = new Ticket[20];
+            private Projector projector = new Projector();
+
+            public void AddTicket(Ticket t)
+            {
+                for (int i = 0; i < tickets.Length; i++)
+                {
+                    if (tickets[i] == null)
+                    {
+                        tickets[i] = t;
+                        break;
+                    }
+                }
+            }
+
+            public void PrintAllTickets()
+            {
+                Console.WriteLine("\n========== All Tickets ==========");
+
+                foreach (var t in tickets)
+                {
+                    if (t != null)
+                        t.PrintTicket();
+                }
+            }
+
+            public void OpenCinema()
+            {
+                Console.WriteLine("\n========== Cinema Opened ==========");
+                projector.Start();
+            }
+
+            public void CloseCinema()
+            {
+                Console.WriteLine("\n========== Cinema Closed ==========");
+                projector.Stop();
+            }
             #endregion
 
 
