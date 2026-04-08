4845f115ed4d68bc719234824f19e2c484607b9d
AhmedShehap99 <55rrtyy7@gmail.com>
2026-04-08 20:32:12 +0100

    part2

--- OOP Assignment 4/Program.cs
+++ OOP Assignment 4/Program.cs
@@ -96,10 +96,184 @@ namespace OOP_Assignment_4
 
 
 
+            //namespace Assignment_04_OOP.BookingTicket.Inheritance
+            //        {
+            //            internal class Projector
+            //            {
+            //                public void Start()
+            //                {
+            //                    Console.WriteLine("Projector started.");
+            //                }
+
+            //                public void Stop()
+            //                {
+            //                    Console.WriteLine("Projector stopped.");
+            //                }
 
 
 
 
-        }
-    }
-}
+
+
+            //namespace Assignment_04_OOP.BookingTicket.Inheritance
+            //        {
+            //            internal class StandardTicket : Ticket
+            //            {
+
+
+            //                public string seatNumber { get; set; }
+            //                public StandardTicket(string movieName, decimal price, string seat) : base(movieName, price)
+            //                {
+            //                    seatNumber = seat;
+
+            //                }
+
+
+
+
+
+
+
+            //                public override void PrintTicket()
+            //                {
+            //                    base.PrintTicket();
+            //                    Console.WriteLine($"  Seat: {seatNumber}");
+            //                }
+
+            //            }
+            //        }
+            //        }
+            //}
+
+            //namespace Assignment_04_OOP.BookingTicket.Inheritance
+            //        {
+            //            internal class VIPTicket : Ticket
+
+            //            {
+            //                public bool LoungeAccess { get; set; }
+
+            //                public decimal ServiceFee { get; } = 50;
+            //                public VIPTicket(string movieName, decimal price, bool louAccs) : base(movieName, price + 50)
+            //                {
+
+            //                    LoungeAccess = louAccs;
+            //                }
+
+            //                public override void PrintTicket()
+            //                {
+            //                    base.PrintTicket();
+            //                    Console.WriteLine($"  Lounge: {(LoungeAccess ? "Yes" : "No")} | Service Fee: {ServiceFee} EGP");
+            //                }
+
+
+            //            }
+
+            //internal class Ticket
+            //{
+
+            //    public string MovieName { get; set; }
+            //    public decimal Price { get; private set; }
+
+            //    public int TicketId { get; }
+
+            //    private static int counter = 0;
+
+            //    public Ticket(string movieName, decimal price)
+            //    {
+            //        if (price <= 0)
+            //        {
+            //            throw new ArgumentException("Price must be greater than zero.");
+            //        }
+
+
+
+
+
+            //        MovieName = movieName;
+            //        Price = price;
+
+            //        counter++;
+            //        TicketId = counter;
+
+
+            //    }
+
+            //    public static int GetTotalTickets()
+            //    {
+            //        return counter;
+            //    }
+
+            //    public decimal PriceAfterTax()
+            //    {
+            //        return Price * 1.14m;
+            //    }
+
+
+            //    public void SetPrice(decimal price)
+            //    {
+            //        if (price > 0)
+            //            Price = price;
+            //    }
+
+            //    public void SetPrice(decimal basePrice, decimal multiplier)
+            //    {
+            //        if (basePrice > 0 && multiplier > 0)
+            //            Price = basePrice * multiplier;
+            //    }
+
+
+
+            //    public virtual void PrintTicket()
+            //    {
+            //        Console.WriteLine($"Ticket #{TicketId} | {MovieName} | Price: {Price} EGP | After Tax: {PriceAfterTax:F2} EGP");
+            //    }
+
+
+
+
+
+
+        //    Cinema cinema = new Cinema();
+        //    cinema.OpenCinema();
+
+
+
+        //    Console.WriteLine("\n========== SetPrice Test ==========");
+
+
+        //    StandardTicket t1 = new StandardTicket("Inception", 120, "A-5");
+
+
+        //    Console.WriteLine("Setting price directly: 150");
+        //    t1.SetPrice(150);
+
+
+        //    Console.WriteLine("Setting price with multiplier: 100 x 1.5 = 150");
+        //    t1.SetPrice(100, 1.5m);
+
+
+        //    VIPTicket t2 = new VIPTicket("Avengers", 200, true);
+        //    IMAXTicket t3 = new IMAXTicket("Dune", 180, false);
+
+
+
+        //    cinema.AddTicket(t1);
+        //    cinema.AddTicket(t2);
+        //    cinema.AddTicket(t3);
+
+        //    cinema.PrintAllTickets();
+
+        //    ProcessTicket(t2);
+
+        //    cinema.CloseCinema();
+        //}
+
+
+
+        //static void ProcessTicket(Ticket t)
+        //    {
+        //        Console.WriteLine("\n========== Process Single Ticket ==========");
+        //        t.PrintTicket();
+        //    }
+
+
